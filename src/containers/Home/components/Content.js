a04fdfbfed6aec6345ae16daf8c743a35cfd10dd
chnbohwr <chnbohwr@gmail.com>
2017-08-26 21:57:52 +0800

    add game stage

--- src/containers/Home/components/Content.js
+++ src/containers/Home/components/Content.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import './Content.less';
+
+const gameStageArea = {
+  width: 50,
+  height: 20,
+};
 
 export default class Content extends Component {
+  constructor() {
+    super();
+    this.state = {
+      stage: this.generateStage(),
+    };
+  }
+  generateStage = () => Array.from(Array(gameStageArea.height))
+    .map((valRow, rowIndex) => (
+      Array.from(Array(gameStageArea.width))
+        .map((valCol, colIndex) => (
+          { id: (rowIndex * gameStageArea.width) + colIndex, key: `block${(rowIndex * gameStageArea.width) + colIndex}` }
+        ))
+    ));
+
   render() {
     return (
       <div className="content">
-        <h1>Hello, We are ReactMaker!</h1>
-        <h2>This is Content Component!</h2>
+        <div id="stage">
+          {
+            this.state.stage.map((stageRow, rowIndex) => <div className="stageRow" key={`stageRow${rowIndex}`}>
+              {stageRow.map(stageCol => <div className="stageCol" key={stageCol.key} />)}
+            </div>)
+          }
+        </div>
       </div>
     );
   }
